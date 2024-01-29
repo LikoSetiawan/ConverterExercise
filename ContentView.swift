e47c875b9c0d48b84001a0df89a8666a759b1040
LikoSetiawan <128354782+LikoSetiawan@users.noreply.github.com>
2024-01-29 18:37:38 +0800

    Initial commit

---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ConverterExercise
+//
+//  Created by Liko Setiawan on 29/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ConverterExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterExerciseApp.swift
+//  ConverterExercise
+//
+//  Created by Liko Setiawan on 29/01/24.
+//
+
+import SwiftUI
+
+@main
+struct ConverterExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
